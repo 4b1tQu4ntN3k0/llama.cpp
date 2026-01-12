a05859d6ecfda81a468e6b58065c54d69fb050c3
4b1tQu4ntN3k0 <2200680109@qq.com>
2026-01-12 20:31:32 +0800

    add build script

---
+++ scripts/pipo/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "$1" == "debug" ]; then
+    echo "Building Debug version..."
+    mkdir -p build-debug
+    cd build-debug
+    cmake .. -DCMAKE_BUILD_TYPE=Debug -DGGML_CUDA=ON
+    cmake --build . --target llama-simple -j 8
+    echo "Debug build complete. Executable is in build-debug/bin/"
+elif [ "$1" == "release" ]; then
+    echo "Building Release version..."
+    mkdir -p build-release
+    cd build-release
+    cmake .. -DGGML_CUDA=ON
+    cmake --build . --target llama-simple -j 8
+    echo "Release build complete. Executable is in build-release/bin/"
+else
+    echo "Usage: $0 [debug|release]"
+    exit 1
+fi
