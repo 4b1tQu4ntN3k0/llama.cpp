42f774e8008c0e6a5615e168573d0ea069d1a7f9
4b1tQu4ntN3k0 <2200680109@qq.com>
2026-01-12 23:34:22 +0800

    remove unused 'layer' code

--- src/llama-context.h
+++ src/llama-context.h
@@ -126,12 +126,6 @@ struct llama_context {
             llama_memory_context_i * mctx,
                        ggml_status & ret);
 
-    llm_graph_result * process_ubatch_pipo(
-                const llama_ubatch & ubatch,
-                    llm_graph_type   gtype,
-            llama_memory_context_i * mctx,
-                       ggml_status & ret);
-
     int encode(const llama_batch & batch_inp);
     int decode(const llama_batch & batch_inp);
 
@@ -237,7 +231,6 @@ public:
     // reserve a graph with a dummy ubatch of the specified size
     ggml_cgraph * graph_reserve(
         uint32_t n_tokens, uint32_t n_seqs, uint32_t n_outputs, const llama_memory_context_i * mctx, bool split_only = false, size_t * sizes = nullptr);
-    ggml_status graph_reserve_pipo(uint32_t n_tokens, uint32_t n_seqs, uint32_t n_outputs, const llama_memory_context_i * mctx, bool split_only = false);
 
     bool set_sampler(llama_seq_id seq_id, llama_sampler * sampler);
 
