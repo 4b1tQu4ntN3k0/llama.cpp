cbcd4b8128a7cb5c5309d41651db6b54e785467a
4b1tQu4ntN3k0 <2200680109@qq.com>
2026-01-29 01:14:28 +0800

    rebase & update llama-bench

--- examples/simple/simple.cpp
+++ examples/simple/simple.cpp
@@ -126,12 +126,10 @@ void pipo_tensor_layout(std::vector<llama_model_tensor_buft_override>& overrides
 
     overrides.clear();
 
+    // qwen3-14B
     overrides.push_back({ "blk\\.([0-9]|[1-3][0-9])\\.ffn_down\\.weight", cuda_host });
     overrides.push_back({ "blk\\.([0-9]|[1-3][0-9])\\.ffn_up\\.weight", cuda_host });
-
     overrides.push_back({ "^token_embd\\.weight$", cuda_host });
-    // overrides.push_back({ "^output\\.weight$", cuda_host });
-    // overrides.push_back({ "^output_norm\\.weight$", cuda_host });
     overrides.push_back({ ".*", cuda });
 
     // Terminate with nullptr
@@ -142,11 +140,10 @@ void pipo_assign_offload(std::vector<const char*>& prefill_offload, std::vector<
     prefill_offload.clear();
     decode_offload.clear();
     
+    // qwen3-14B
     prefill_offload.push_back("blk\\.([0-9]|[1-3][0-9])\\.ffn_down\\.weight");
     prefill_offload.push_back("blk\\.([0-9]|[1-3][0-9])\\.ffn_up\\.weight");
-
-    decode_offload.push_back("blk\\.(3|7|11|15|19|23|27|31|35|39)\\.ffn_down\\.weight");
-    // decode_offload.push_back("blk\\.([0-9]|[1-3][0-9])\\.ffn_up\\.weight");
+    decode_offload.push_back("blk\\.(2|5|8|11|14|17|20|23|26|29|32|35|38)\\.ffn_down\\.weight");
 }
 
 int main(int argc, char ** argv) {
