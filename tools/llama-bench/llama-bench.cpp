b01bcf28326762df21c09924e37dc8d86a559b6b
4b1tQu4ntN3k0 <2200680109@qq.com>
2026-01-29 01:14:28 +0800

    rebase & update llama-bench

--- tools/llama-bench/llama-bench.cpp
+++ tools/llama-bench/llama-bench.cpp
@@ -23,6 +23,7 @@
 #include "common.h"
 #include "download.h"
 #include "fit.h"
+#include "pipo.h"
 #include "ggml.h"
 #include "llama.h"
 
@@ -347,7 +348,6 @@ struct cmd_params {
     std::vector<bool>                no_op_offload;
     std::vector<bool>                no_host;
     std::vector<bool>                enable_pipo;
-    std::vector<int>                 n_cpu_layers_per_split;
     std::vector<size_t>              fit_params_target;
     std::vector<uint32_t>            fit_params_min_ctx;
     ggml_numa_strategy               numa;
@@ -393,7 +393,6 @@ static const cmd_params cmd_params_defaults = {
     /* no_op_offload        */ { false },
     /* no_host              */ { false },
     /* enable_pipo          */ { false },
-    /* n_cpu_layers_per_split*/{ 3 },
     /* fit_params_target    */ { 0 },
     /* fit_params_min_ctx   */ { 0 },
     /* numa                 */ GGML_NUMA_STRATEGY_DISABLED,
@@ -467,8 +466,6 @@ static void print_usage(int /* argc */, char ** argv) {
     printf("  --no-host <0|1>                             (default: %s)\n", join(cmd_params_defaults.no_host, ",").c_str());
     printf("  --pipo <0|1>                              (default: %s)\n",
            join(cmd_params_defaults.enable_pipo, ",").c_str());
-    printf("  --n-cpu-layers-per-split <n>              (default: %s)\n",
-           join(cmd_params_defaults.n_cpu_layers_per_split, ",").c_str());
     printf("\n");
     printf(
         "Multiple values can be given for each parameter by separating them with ','\n"
@@ -845,13 +842,6 @@ static cmd_params parse_cmd_params(int argc, char ** argv) {
                 }
                 auto p = string_split<bool>(argv[i], split_delim);
                 params.enable_pipo.insert(params.enable_pipo.end(), p.begin(), p.end());
-            } else if (arg == "--n-cpu-layers-per-split") {
-                if (++i >= argc) {
-                    invalid_param = true;
-                    break;
-                }
-                auto p = parse_int_range(argv[i]);
-                params.n_cpu_layers_per_split.insert(params.n_cpu_layers_per_split.end(), p.begin(), p.end());
             } else if (arg == "-ts" || arg == "--tensor-split") {
                 if (++i >= argc) {
                     invalid_param = true;
@@ -1123,9 +1113,6 @@ static cmd_params parse_cmd_params(int argc, char ** argv) {
     if (params.enable_pipo.empty()) {
         params.enable_pipo = cmd_params_defaults.enable_pipo;
     }
-    if (params.n_cpu_layers_per_split.empty()) {
-        params.n_cpu_layers_per_split = cmd_params_defaults.n_cpu_layers_per_split;
-    }
     if (params.n_threads.empty()) {
         params.n_threads = cmd_params_defaults.n_threads;
     }
@@ -1176,7 +1163,6 @@ struct cmd_params_instance {
     bool               no_op_offload;
     bool               no_host;
     bool               enable_pipo;
-    int                n_cpu_layers_per_split;
     size_t             fit_target;
     uint32_t           fit_min_ctx;
 
@@ -1193,8 +1179,7 @@ struct cmd_params_instance {
         mparams.use_mmap      = use_mmap;
         mparams.use_direct_io = use_direct_io;
         mparams.no_host       = no_host;
-        mparams.enable_pipo  = enable_pipo;
-        mparams.n_cpu_layers_per_split = n_cpu_layers_per_split;
+        mparams.enable_pipo   = enable_pipo;
 
         if (n_cpu_moe <= 0) {
             if (tensor_buft_overrides.empty()) {
@@ -1243,7 +1228,6 @@ struct cmd_params_instance {
                devices == other.devices &&
                no_host == other.no_host &&
                enable_pipo == other.enable_pipo &&
-               n_cpu_layers_per_split == other.n_cpu_layers_per_split &&
                vec_tensor_buft_override_equal(tensor_buft_overrides, other.tensor_buft_overrides);
     }
 
@@ -1259,12 +1243,8 @@ struct cmd_params_instance {
         cparams.flash_attn_type = flash_attn ? LLAMA_FLASH_ATTN_TYPE_ENABLED : LLAMA_FLASH_ATTN_TYPE_DISABLED;
         cparams.embeddings      = embeddings;
         cparams.op_offload      = !no_op_offload;
-        cparams.swa_full        = false;
         cparams.enable_pipo     = enable_pipo;
-        cparams.n_cpu_layers_per_split = n_cpu_layers_per_split;
-        if(enable_pipo){
-            cparams.op_offload = false;
-        }
+        cparams.swa_full        = false;
 
         return cparams;
     }
@@ -1289,7 +1269,6 @@ static std::vector<cmd_params_instance> get_cmd_params_instances(const cmd_param
     for (const auto & dio : params.use_direct_io)
     for (const auto & noh : params.no_host)
     for (const auto & pipo : params.enable_pipo)
-    for (const auto & nclps : params.n_cpu_layers_per_split)
     for (const auto & embd : params.embeddings)
     for (const auto & nopo : params.no_op_offload)
     for (const auto & nb : params.n_batch)
@@ -1335,10 +1314,25 @@ static std::vector<cmd_params_instance> get_cmd_params_instances(const cmd_param
                 /* .no_op_offload= */ nopo,
                 /* .no_host      = */ noh,
                 /* .enable_pipo  = */ pipo,
-                /* .n_cpu_layers_per_split = */ nclps,
                 /* .fit_target   = */ fpt,
                 /* .fit_min_ctx  = */ fpc,
             };
+            if(pipo) {
+                ggml_backend_buffer_type_t cuda = nullptr, cuda_host = nullptr;
+                for (size_t i = 0; i < ggml_backend_dev_count(); ++i) {
+                    auto * dev = ggml_backend_dev_get(i);
+                    auto * buft = ggml_backend_dev_buffer_type(dev);
+                    if (buft) {
+                        auto name = ggml_backend_buft_name(buft);
+                        if (strstr(name, "CUDA")){
+                            cuda = buft;
+                            cuda_host = ggml_backend_dev_host_buffer_type(dev);
+                            break;
+                        }
+                    }
+                }
+                pipo_tensor_layout(instance.tensor_buft_overrides, cuda, cuda_host);
+            }
             instances.push_back(instance);
         }
 
@@ -1374,10 +1368,25 @@ static std::vector<cmd_params_instance> get_cmd_params_instances(const cmd_param
                 /* .no_op_offload= */ nopo,
                 /* .no_host      = */ noh,
                 /* .enable_pipo  = */ pipo,
-                /* .n_cpu_layers_per_split = */ nclps,
                 /* .fit_target   = */ fpt,
                 /* .fit_min_ctx  = */ fpc,
             };
+            if(pipo) {
+                ggml_backend_buffer_type_t cuda = nullptr, cuda_host = nullptr;
+                for (size_t i = 0; i < ggml_backend_dev_count(); ++i) {
+                    auto * dev = ggml_backend_dev_get(i);
+                    auto * buft = ggml_backend_dev_buffer_type(dev);
+                    if (buft) {
+                        auto name = ggml_backend_buft_name(buft);
+                        if (strstr(name, "CUDA")){
+                            cuda = buft;
+                            cuda_host = ggml_backend_dev_host_buffer_type(dev);
+                            break;
+                        }
+                    }
+                }
+                pipo_tensor_layout(instance.tensor_buft_overrides, cuda, cuda_host);
+            }
             instances.push_back(instance);
         }
 
@@ -1413,10 +1422,25 @@ static std::vector<cmd_params_instance> get_cmd_params_instances(const cmd_param
                 /* .no_op_offload= */ nopo,
                 /* .no_host      = */ noh,
                 /* .enable_pipo  = */ pipo,
-                /* .n_cpu_layers_per_split = */ nclps,
                 /* .fit_target   = */ fpt,
                 /* .fit_min_ctx  = */ fpc,
             };
+            if(pipo) {
+                ggml_backend_buffer_type_t cuda = nullptr, cuda_host = nullptr;
+                for (size_t i = 0; i < ggml_backend_dev_count(); ++i) {
+                    auto * dev = ggml_backend_dev_get(i);
+                    auto * buft = ggml_backend_dev_buffer_type(dev);
+                    if (buft) {
+                        auto name = ggml_backend_buft_name(buft);
+                        if (strstr(name, "CUDA")){
+                            cuda = buft;
+                            cuda_host = ggml_backend_dev_host_buffer_type(dev);
+                            break;
+                        }
+                    }
+                }
+                pipo_tensor_layout(instance.tensor_buft_overrides, cuda, cuda_host);
+            }
             instances.push_back(instance);
         }
     }
@@ -1457,7 +1481,6 @@ struct test {
     bool                     no_op_offload;
     bool                     no_host;
     bool                     enable_pipo;
-    int                      n_cpu_layers_per_split;
     size_t                   fit_target;
     uint32_t                 fit_min_ctx;
     int                      n_prompt;
@@ -1499,7 +1522,6 @@ struct test {
         no_op_offload  = inst.no_op_offload;
         no_host        = inst.no_host;
         enable_pipo    = inst.enable_pipo;
-        n_cpu_layers_per_split = inst.n_cpu_layers_per_split;
         fit_target     = inst.fit_target;
         fit_min_ctx    = inst.fit_min_ctx;
         n_prompt       = inst.n_prompt;
@@ -1559,9 +1581,10 @@ struct test {
             "type_k",         "type_v",         "n_gpu_layers",  "n_cpu_moe",      "split_mode",
             "main_gpu",       "no_kv_offload",  "flash_attn",    "devices",        "tensor_split",
             "tensor_buft_overrides",            "use_mmap",      "use_direct_io",  "embeddings",
-            "no_op_offload",  "no_host",        "enable_pipo","n_cpu_layers_per_split",
+            "no_host",        "enable_pipo","n_cpu_layers_per_split",
             "fit_target",     "fit_min_ctx",
             "n_prompt",       "n_gen",          "n_depth",
+
             "test_time",      "avg_ns",         "stddev_ns",     "avg_ts",         "stddev_ts"
         };
         return fields;
@@ -1573,7 +1596,7 @@ struct test {
         if (field == "build_number" || field == "n_batch" || field == "n_ubatch" || field == "n_threads" ||
             field == "poll" || field == "model_size" || field == "model_n_params" || field == "n_gpu_layers" ||
             field == "main_gpu" || field == "n_prompt" || field == "n_gen" || field == "n_depth" || field == "avg_ns" ||
-            field == "stddev_ns" || field == "no_op_offload" || field == "n_cpu_moe" || field == "n_cpu_layers_per_split" ||
+            field == "stddev_ns" ||  field == "n_cpu_layers_per_split" ||
             field == "fit_target" || field == "fit_min_ctx") {
             return INT;
         }
@@ -1656,7 +1679,6 @@ struct test {
                                             std::to_string(no_op_offload),
                                             std::to_string(no_host),
                                             std::to_string(enable_pipo),
-                                            std::to_string(n_cpu_layers_per_split),
                                             std::to_string(fit_target),
                                             std::to_string(fit_min_ctx),
                                             std::to_string(n_prompt),
@@ -1856,9 +1878,6 @@ struct markdown_printer : public printer {
         if (field == "enable_pipo") {
             return 4;
         }
-        if (field == "n_cpu_layers_per_split") {
-            return 3;
-        }
 
         int width = std::max((int) field.length(), 10);
 
@@ -1901,9 +1920,6 @@ struct markdown_printer : public printer {
         }
         if (field == "enable_pipo") {
             return "pipo";
-        }
-        if (field == "n_cpu_layers_per_split") {
-            return "cps";
         }
         if (field == "devices") {
             return "dev";
@@ -2000,9 +2016,6 @@ struct markdown_printer : public printer {
         }
         if (params.enable_pipo.size() > 1 || params.enable_pipo != cmd_params_defaults.enable_pipo) {
             fields.emplace_back("enable_pipo");
-        }
-        if (params.n_cpu_layers_per_split.size() > 1 || params.n_cpu_layers_per_split != cmd_params_defaults.n_cpu_layers_per_split) {
-            fields.emplace_back("n_cpu_layers_per_split");
         }
         if (params.fit_params_target.size() > 1 || params.fit_params_target != cmd_params_defaults.fit_params_target) {
             fields.emplace_back("fit_target");
@@ -2328,6 +2341,13 @@ int main(int argc, char ** argv) {
                 fprintf(stderr, "%s: error: failed to load model '%s'\n", __func__, inst.model.c_str());
                 return 1;
             }
+
+            if (inst.enable_pipo) {
+                std::vector<const char*> p_offload, d_offload;
+                pipo_assign_offload(p_offload, d_offload);
+                llama_model_set_offload(lmodel, p_offload.data(), d_offload.data(), p_offload.size(), d_offload.size());
+            }
+
             prev_inst = &inst;
         }
 
