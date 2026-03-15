c017ba730afb75007045e63e729353658a6b7e5a
4b1tQu4ntN3k0 <2200680109@qq.com>
2026-03-15 22:12:42 +0800

    update build script

--- scripts/pipo/build.sh
+++ scripts/pipo/build.sh
@@ -5,14 +5,14 @@ if [ "$1" == "debug" ]; then
     mkdir -p build-debug
     cd build-debug
     cmake .. -DCMAKE_BUILD_TYPE=Debug -DGGML_CUDA=ON
-    cmake --build . --target llama-simple -j 8
+    cmake --build . --target llama-simple llama-pipo-perf llama-pipo-override -j 8
     echo "Debug build complete. Executable is in build-debug/bin/"
 elif [ "$1" == "release" ]; then
     echo "Building Release version..."
     mkdir -p build-release
     cd build-release
     cmake .. -DGGML_CUDA=ON
-    cmake --build . --target llama-simple -j 8
+    cmake --build . --target llama-simple llama-pipo-perf llama-pipo-override -j 8
     echo "Release build complete. Executable is in build-release/bin/"
 else
     echo "Usage: $0 [debug|release]"
